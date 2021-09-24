9fb2293a2808cc48e987181fbdc24271d3b57e52
YohannM <yohannmarmonier@gmail.com>
2021-09-24 11:57:12 +0100

    bug fix cmd script

--- create_on_mssql.cmd
+++ create_on_mssql.cmd
@@ -20,10 +20,10 @@ echo .
 echo MSSQL Configuration...
 echo.
 echo Activating Ole Automations Components :
-sqlcmd -S %server% -d %db% -U %username% -P %pwd% -i tables/ACTIVATE_OLE_AUTOMATION_COMPONENTS.sql
+sqlcmd -S %server% -d %db% -U %username% -P %pwd% -i config/ACTIVATE_OLE_AUTOMATION_COMPONENTS.sql
 echo.
 echo Checking Master Key :
-sqlcmd -S %server% -d %db% -U %username% -P %pwd% -i tables/CREATE_MASTER_KEY.sql
+sqlcmd -S %server% -d %db% -U %username% -P %pwd% -i config/CREATE_MASTER_KEY.sql
 echo.
 echo.
 echo Table creation...
@@ -32,7 +32,7 @@ echo Table RPA_ORCHESTRATORAPI_ASSETS :
 sqlcmd -S %server% -d %db% -U %username% -P %pwd% -i tables/CREATE_TABLE___RPA_ORCHESTRATORAPI_ASSETS.sql
 echo.
 echo Filling RPA_ORCHESTRATORAPI_ASSETS with app info :
-sqlcmd -S %server% -d %db% -U %username% -P %pwd% -i tables/FILL_TABLE.sql
+sqlcmd -S %server% -d %db% -U %username% -P %pwd% -i config/FILL_TABLE.sql
 echo.
 echo Table RPA_ORCHESTRATORAPI_FAILEDJOBLAUNCHING :
 sqlcmd -S %server% -d %db% -U %username% -P %pwd% -i tables/CREATE_TABLE___RPA_ORCHESTRATORAPI_FAILEDJOBLAUNCHING.sql
